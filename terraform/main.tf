655d1d8b1ce12586a2a75c1bef2ab68c46dd75c0
Manohar Reddy Voladri <92422839+manoharreddyvoladri@users.noreply.github.com>
2025-04-06 16:28:14 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+}
+
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "medusa_db" {
+  identifier         = "medusadb"
+  allocated_storage  = 20
+  engine             = "postgres"
+  engine_version     = "14.1"
+  instance_class     = "db.t3.micro"
+  name               = "medusadb"
+  username           = "medusa"
+  password           = "medusapass123"
+  skip_final_snapshot = true
+  publicly_accessible = true
+  vpc_security_group_ids = [aws_security_group.ecs_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnets.name
+}
+
+resource "aws_db_subnet_group" "db_subnets" {
+  name       = "medusa-db-subnet-group"
+  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+}
+
+# ECS + Fargate and ALB code will be added in next step
